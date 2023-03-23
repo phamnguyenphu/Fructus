fe081f322e34d2b7be13a9fece6855a88d9361b1
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-03-23 14:21:02 +0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Pham Nguyen Phu on 23/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
