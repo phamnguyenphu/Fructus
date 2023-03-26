9fd1214fe46b295454a654db3373916da7f24fb8
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-03-26 13:56:42 +0700

    create data model and fetch data

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -12,31 +12,32 @@ struct FruitCardView: View {
     
     @State private var isAnimating: Bool = false
     
+    var fruit: Fruit
+    
     // MARK: - FUNCTIOM
     
     // MARK: - BODY
     
     var body: some View {
         ZStack {
-            LinearGradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")], startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: .init(colors: [fruit.gradientColors[0], fruit.gradientColors[1]]), startPoint: .top, endPoint: .bottom)
             
             VStack(spacing: 20) {
                 // FRUIT: IMAGE
-                Image("blueberry")
+                Image(fruit.image)
                     .resizable()
                     .scaledToFit()
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.2), radius: 8, x: 6, y: 8)
-                    .scaleEffect(isAnimating ? 1.0 : 0.6)
                 
                 // FRUIT: TITLE
-                Text("Blueberry")
+                Text(fruit.title)
                     .foregroundColor(.white)
                     .fontWeight(.heavy)
                     .font(.largeTitle)
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.2), radius: 2, x: 2, y: 2)
                 
                 // FRUIT: HEADLINE
-                Text("Blueberries are sweet, nutritious and wildly popular fruit all over the world.")
+                Text(fruit.headline)
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal)
@@ -45,18 +46,14 @@ struct FruitCardView: View {
                 // FRUIT: BUTTON
                 StartButtonView()
             } //: VSTACK
-            
-            //            .background {
-            //                LinearGradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")], startPoint: .top, endPoint: .bottom)
-            //        }
         } //: ZSTACK
         .cornerRadius(20)
         .padding(.horizontal, 20)
-        .onAppear {
-            withAnimation(.easeOut(duration: 0.5)) {
-                isAnimating = true
-            }
-        }
+        //        .onAppear {
+        //            withAnimation(.easeOut(duration: 0.5)) {
+        //                isAnimating = true
+        //            }
+        //        }
     }
 }
 
@@ -64,7 +61,7 @@ struct FruitCardView: View {
 
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitCardView()
+        FruitCardView(fruit: fruitsData[1])
             .previewLayout(.fixed(width: 320, height: 640))
     }
 }
