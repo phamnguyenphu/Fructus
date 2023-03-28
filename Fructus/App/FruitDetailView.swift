00b7059ce41086b8807822a935de231992da8f61
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-03-28 13:38:50 +0700

    create a detail view layout in SwiftUI part2

--- Fructus/App/FruitDetailView.swift
+++ Fructus/App/FruitDetailView.swift
@@ -33,6 +33,7 @@ struct FruitDetailView: View {
                             .font(.headline)
                             .multilineTextAlignment(.leading)
                         // NUTRIENTS
+                        FruitNutrientsView(fruit: fruit)
                         
                         // SUBHEADLINE
                         Text("Learn more about \(fruit.title)".uppercased())
